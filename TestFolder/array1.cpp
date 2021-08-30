d9429f33d0d3f9f77b3c0c5dd7f70e83bc0be6f1
KarishaDel <karsishadelrosario@gmail.com>
2021-08-30 15:17:58 -0700

    Test

---
+++ TestFolder/array1.cpp
@@ -0,0 +1,38 @@
+/*find the elemnt that has increasing order in three consecutive elemnets i.e: 4 < 6 < 7 */
+
+#include <iostream>
+using std::cout;
+using std::endl;
+
+int main()
+{
+	//const int n=10;
+	int num_line[10]{ 5,7,2,4,6,7,10,1,3,9 };
+	//int min = num_line[0];
+	int temp_min = 0;
+	//int skip = 0;
+
+	for (int i = 1; i < 10; i++)
+	{
+		//for (int j = i + 1; j < 10; j++)
+		//{
+			if (num_line[i-1] < num_line[i])
+			{
+				temp_min = num_line[i];
+			}
+			if (temp_min < num_line[i + 1])
+			{
+				cout << temp_min;
+			}
+			
+		//}
+		//skip++;
+		//if (num_line[i - 1] < num_line[i])
+		//{
+		//	temp_min = i;
+		//	min = num_line[i];
+		//}
+		//cout << "Min: " << skip << endl;
+	}
+
+}
