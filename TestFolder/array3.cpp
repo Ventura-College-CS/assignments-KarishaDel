db322a0037bbbf6eb12fddd184750dbe4453bb70
KarishaDel <karsishadelrosario@gmail.com>
2021-09-13 14:52:48 -0700

    another one

--- TestFolder/array3.cpp
+++ TestFolder/array3.cpp
@@ -1,35 +1,41 @@
-#include<iostream>
+#include <iostream>
 #include <cstring>
 using namespace std;
 
-int main() 
+int main()
 {
-    // string mainstr = "Chocolate";
-    // string substr = "col";
 
-    // int spot;
+	// string mainstr="chocolate";
+	// string substr= "col";
 
-    // spot = mainstr.find(substr);
+	char mainstr[10]="chocolate";
+	char substr[10]="col";
+	int pos, i, j;
+	int flag=0;
 
-    // cout << "Found in spot: " << spot << endl;
+	// pos = mainstr.find(substr) ; 
+	cout << sizeof(mainstr) << endl; 
+	cout << sizeof(substr) << endl;
 
-    char mainstr [10] = "chocolate";
-    char substr[10] = "col";
-    int i, j;
+	cout << strlen(substr) << endl;
+	cout << strlen(mainstr) << endl; 
 
-    cout << strlen(substr) << endl;
-    cout << strlen(mainstr) << endl;
+	// cout << " found at position " << pos << endl;
 
-    for(int i = 0; i <= strlen(mainstr) - strlen(substr); i++)
-    {
-        for(int j = 0; j < strlen(substr); j++)
-        {
-            if(substr[j] == mainstr[i+j]);
-            break;
-        }
-         if (j == strlen(substr))
-         cout << "matched" << i << endl;
-
-    }
-
-}
+	for(  i =0; i < strlen(mainstr) - strlen(substr) + 1 ; i++)
+	{
+		for (  j=0; j < strlen(substr); j++)
+		{
+			if ( substr[j] != mainstr[i+j])
+				break;
+		}
+		// if ( full iteration of inside for loop)
+		if ( j == strlen(substr)){
+			flag = 1;
+			cout << " matched at the position " << i << endl;
+		}
+	
+	}
+    if(!flag)
+    cout << "We did not find a match." << endl;
+}    
