ef4d6e51fb926f6751ebe41dbea8d9e33e1cfccc
KarishaDel <karsishadelrosario@gmail.com>
2021-09-20 13:52:56 -0700

    send

--- TestFolder/vector.cpp
+++ TestFolder/vector.cpp
@@ -8,10 +8,23 @@ void printVec(vector<int>);
 
 int main()
 {
-    vector<int> number = {10,13,5,7,25};
+    vector<int>::iterator it;
+    vector<int> number = {12,15,19,21,25, 27,29,33,37,43};
     printVec(number);
     sort(number.begin(), number.end());
+    int new_num;
 
+    cout << "Enter the new number: ";
+    cin >> new_num;
+
+    for( it=number.begin(); it< number.end(); it++)
+    {
+        if(*it > new_num)
+        break;
+    }
+
+    number.insert(it ,new_num);
+    printVec(number);
 }
 
 void printVec(vector<int> vec)
@@ -19,6 +32,8 @@ void printVec(vector<int> vec)
     vector<int>::iterator it;
         for (it = vec.begin(); it < vec.end(); it++)
     {
-        cout << *it << endl; // * is a pointer
+        cout << *it << "\t"; // * is a pointer
     }
+    cout << endl;
+
 }
