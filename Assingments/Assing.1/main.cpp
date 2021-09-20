ed6485fee3e4bee9d8b76f67efcd5c5baf6e1b12
KarishaDel <karsishadelrosario@gmail.com>
2021-09-20 13:02:31 -0700

    trying

--- Assingments/Assing.1/main.cpp
+++ Assingments/Assing.1/main.cpp
@@ -1,5 +1,9 @@
 #include	<iostream>
+#include <fstream>
+
 using namespace	std;
+
+void stu_find();
 
 struct student
 {
@@ -13,9 +17,13 @@ int	main()
 {
 	student	s1;
 	student 	s2 = {10001, "Mary", "Lafayette"};
+	ifstream rdfile;
+
+	rdfile.open("students.txt");
 
 	cout << "Enter the student's information (id, name, address)\n";
 	cin >> s1.id >> s1.name >> s1.address ;
 	cout << "Student Info S1\n" << s1. id << "\t" << s1.name << "\t" << s1.address << endl;
 	
 	cout << "Student Info S2\n" << s2. id << "\t" << s2.name << "\t" << s2.address << endl;
+}
