5f279190306bb08df5826278506a41586d5408b7
KarishaDel <karsishadelrosario@gmail.com>
2021-09-27 13:24:45 -0700

    class work

---
+++ 27Sep/pointer.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    // int num;
+    // int num2;
+    // cout << &num << endl; //prints address
+    // cout << &num2 << endl; //prints address, 4 byte difference between the two variables
+
+    int num1 = 100;
+    int num2 = 200;
+    int *prt1; //pointer variable
+    cout << &num1 << endl; 
+    cout << &num2 << endl;
+    prt1 = &num1; //the pointer varible has the address of the other variable
+    cout << prt1 << endl; //print the mem address 
+    cout << *prt1 << endl; //print the VALUE 100
+
+    prt1 = &num2;
+    cout << prt1 << endl; //print the mem address 
+    cout << *prt1 << endl; //print the VALUE 200
+}
