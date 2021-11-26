b552c64d095f15058334fab2cd3bb7a19e38fbd8
KarishaDel <karsishadelrosario@gmail.com>
2021-11-26 11:07:32 -0800

    push

--- MidTerm/midterm1.cpp
+++ MidTerm/midterm1.cpp
@@ -1,5 +1,43 @@
 #include <iostream>
-#include <queue>
-#include <deque>
-#include <list> 
+
 using namespace std;
+
+class Students
+{
+private:
+	int sid;
+	string sname;
+	vector<double> score;
+
+public:
+	Students() : sid(0), sname() {}
+	Students(int id, string name, double s0, double s1, double s2)
+	{
+		sid = id;
+		sname = name;
+		score.push_back(s0);
+		score.push_back(s1);
+		score.push_back(s2);
+	}
+	int getSid() const { return sid; }
+	string getSname() const { return sname; }
+	vector<double> const &getScore() const { return score; }
+	void setSid(int id) { sid = id; }
+	void setSname(string name) { sname = name; }
+	void setScore(double s0, double s1, double s2)
+	{
+		score.clear();
+		score.push_back(s0);
+		score.push_back(s1);
+		score.push_back(s2);
+	}
+    
+	void printStudent() const
+	{
+		cout << "Student ID: " << sid << "\t";
+		cout << "Name: " << sname << "\t";
+		for (int i = 0; i < NUM_SCORES; i++)
+			cout << score[i] << "\t";
+		cout << endl;
+	}
+};
