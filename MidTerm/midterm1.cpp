1cfcc79c1aff37dbb921e2b1788a779da2db683d
KarishaDel <karsishadelrosario@gmail.com>
2021-11-28 15:54:25 -0800

    done

--- MidTerm/midterm1.cpp
+++ MidTerm/midterm1.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include "stack.hpp"
 
+
 using namespace std;
 
 const int NUM_STUDENTS = 10;
@@ -14,13 +15,15 @@ private:
 
 public:
 	Students() : sid(0), sname() {}
-	Students(int id, string name, double s0, double s1, double s2)
+	Students(int id, string name, double s0, double s1, double s2, double s3, double s4)
 	{
 		sid = id;
 		sname = name;
 		score.push_back(s0);
 		score.push_back(s1);
 		score.push_back(s2);
+		score.push_back(s3);
+		score.push_back(s4);
 	}
 
 	int getSid() const { return sid; }
@@ -28,90 +31,50 @@ public:
 	vector<double> const &getScore() const { return score; }
 	void setSid(int id) { sid = id; }
 	void setSname(string name) { sname = name; }
-	void setScore(double s0, double s1, double s2)
+	void setScore(double *sc)
 	{
 		score.clear();
-		score.push_back(s0);
-		score.push_back(s1);
-		score.push_back(s2);
-	}
-    
-	void printStudent() const
-	{
-		cout << "Student ID: " << sid << "\t";
-		cout << "Name: " << sname << "\t";
-		for (int i = 0; i < NUM_SCORES; i++)
-			cout << score[i] << "\t";
-		cout << endl;
+		for (int i=0; i<5; i++)
+		{
+			score.push_back(sc[i]);
+		}
+
 	}
 
-	// void Print()
-	// {
-	// 	Students tmp;
-	// 	vector<double> cs;
-	// 	int i;
-	// 	Stack<Students, 10> st;
-		
-	// 	tmp = st.pop();
-	// 	cs = tmp.getScore();
-	// 	cout << tmp.getSid() << " " << tmp.getSname()<< endl;
-	// 	for(i=0;i<cs.size();i++)
-	// 		{
-	// 			cout << cs[i] <<endl;
-	// 		}
-	// }
 };
 
 
 int main() 
 {
-	Students arr[3] = {s1, s2, s3};
+	Students s[5]; //Students class assing to array
+	string name[] = {"Bill", "Kurt", "John", "Mary", "Shawn"}; //fill in the array for names
+	double score[5][5] = { {100.0, 99.0, 100.0}, {100.0, 99.0, 87.0}, {100.0, 90.0, 100.0}, {88.0, 90.0, 60.0},{100.0, 100.0, 100.0}};
+
 	Students tmp;
 	vector<double> cs;
 	int i;
+	Stack<Students, 10> st;
 
-	s1.setSid(10000001);
-	s1.setSname("Bill");
-	s1.setScore(100, 100, 100);
-	s1.printStudent();
-
-	s2.setSid(10000002);
-	s2.setSname("Kurt");
-	s2.setScore(100, 90, 100);
-	s2.printStudent();
-
-	s3.setSid(10000003);
-	s3.setSname("Greg");
-	s3.setScore(100, 90, 50);
-	s3.printStudent();
-
-	Stack<Students, 10> st; 
-	st.push(s1);
-	st.push(s2);
-	st.push(s3);
-
-	
-	tmp = st.pop();
-	cs = tmp.getScore();
-	cout << tmp.getSid() << " " << tmp.getSname()<< endl;
-	for(i=0;i<cs.size();i++)
+	for (int i=0; i < 5; i++) //this pulls the infor from the class amd places itno the for loop
 	{
-		cout << cs[i] <<endl;
+		s[i].setSid(1000000+i);
+		s[i].setSname(name[i]);
+		s[i].setScore(score[i]);
+		st.push(s[i]);
 	}
-	 
-	tmp = st.pop();
-	cs = tmp.getScore();
-	cout << tmp.getSid() << " "<< tmp.getSname()<< endl;
-	for(i=0;i<cs.size();i++)
+
+	for(int i =0; i < 5; i++) 
+	/*this prints the info out but placing the data in tmp then placing it in cs which allows the next bit of data to go into tmp
+	then it pritns all 3 scores */
 	{
-		cout << cs[i] <<endl;
+		tmp = st.pop();
+		cs = tmp.getScore();
+		cout << tmp.getSid() << " " << tmp.getSname()<< endl;
+		
+		for(int j=0;j<cs.size();j++)
+		{
+			cout << cs[j] <<endl;
+		}
 	}
-	
-	tmp = st.pop();
-	cs = tmp.getScore();
-	cout << tmp.getSid() << " "<< tmp.getSname()<< endl;
-	for(i=0;i<cs.size();i++)
-	{
-		cout << cs[i] <<endl;
-	}
+
 }
