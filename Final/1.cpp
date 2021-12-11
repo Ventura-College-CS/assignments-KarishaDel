060c94ec093a83634b3b92def0ce252e995fa82e
KarishaDel <karsishadelrosario@gmail.com>
2021-12-10 18:52:51 -0800

    PUSH IT

--- Final/1.cpp
+++ Final/1.cpp
@@ -16,10 +16,11 @@ public:
 	{
 		cID = id;
 		cName = name;
-        for (int i; i<10; i++)
-            {
-                int cCredit[i] = rand() % 15;
-            } 
+        cCredit = credit;
+        // for (int i; i<10; i++)
+        //     {
+        //         cCredit[i] = rand() % 15;
+        //     } 
   }
 	
     int getSid() const { return cID; }
@@ -51,50 +52,51 @@ int BinarySearch(Course array[],int first,int last,int target)
 
   int main()
   { 
-    int target = 0; 
     Course array[10];   
     
-    array[0].setCid(1000);
+    array[0].setCid(0);
     array[0].setcName("Spanish");
-    array[0].setcCredit();
+    array[0].setcCredit(4);
     
-    array[1].setCid(1001);
+    array[1].setCid(1);
     array[1].setcName("Geometry");
-    array[1].setcCredit();
+    array[1].setcCredit(4);
     
-    array[2].setCid(1002);
+    array[2].setCid(2);
     array[2].setcName("Psyc");
-    array[2].setcCredit();
+    array[2].setcCredit(5);
     
-    array[3].setCid(1003);
+    array[3].setCid(3);
     array[3].setcName("Lit");
-    array[3].setcCredit();
+    array[3].setcCredit(6);
     
-    array[4].setCid(1004);
+    array[4].setCid(4);
     array[4].setcName("C++");
-    array[4].setcCredit();
+    array[4].setcCredit(2);
     
-    array[5].setCid(1005);
+    array[5].setCid(5);
     array[5].setcName("Java");
-    array[5].setcCredit();
+    array[5].setcCredit(3);
     
-    array[6].setCid(1006);
+    array[6].setCid(6);
     array[6].setcName("Psychology");
-    array[6].setcCredit();
+    array[6].setcCredit(2);
     
-    array[7].setCid(1007);
+    array[7].setCid(7);
     array[7].setcName("Speech");
-    array[7].setcCredit();
+    array[7].setcCredit(5);
     
-    array[8].setCid(1008);
+    array[8].setCid(8);
     array[8].setcName("Physics");
-    array[8].setcCredit();
+    array[8].setcCredit(4);
     
-    array[9].setCid(1009);
+    array[9].setCid(9);
     array[9].setcName("English");
-    array[9].setcCredit();
+    array[9].setcCredit(3);
     
-    cout << "Course ID 1000-1009. Enter ID of course: ";
+    int target = 0; 
+    
+    cout << "Course ID 0-9. Enter ID of course: ";
     cin >> target;
  
     int n = sizeof(array)/sizeof(array[0]);
@@ -103,11 +105,10 @@ int BinarySearch(Course array[],int first,int last,int target)
     int index = BinarySearch(array, first, last, target);
  
     if (index != -1) {
-        cout << "Course is found in the array at position: "<< target; 
-        cout << array[target].getSname()<< endl;
-        cout << array[target].getSid()<< endl;
-        cout << array[target].getCredit() << endl;
-        
+        cout << "Course is found in the array at position: "<< target <<endl; 
+        cout << "Name of Course: "<< array[target].getSname()<< endl;
+        cout << "Course ID 100" << array[target].getSid()<< endl;
+        cout << "Credit for this cousre: " << array[target].getCredit() << endl;  
     }
     else 
     {
---
+++ Practice/p.cpp
@@ -0,0 +1,141 @@
+#include <iostream>
+using namespace std;
+
+class Course
+{
+private: 
+    int  CID;
+    string Cname;
+    int Ccredit;
+public:
+  Course() : CID(0), Cname(), Ccredit() {}
+	Course(int id, string name,int credit)
+	{
+		CID = id;
+		Cname = name;
+        Ccredit = credit;
+  }
+	int getSid() const { return CID; }
+	string getSname() const { return Cname; }
+  int getCredit() const { return Ccredit;}
+	void setCid(int id) { CID = id;}
+  void setCname(string name) { Cname = name;}
+  void setCcredit(int credit) {Ccredit = credit;}
+	
+	// void printCourse() const
+	
+    // {
+    
+    // cout << "Course ID: " << CID << "\n";
+	// 	cout << "Course Name: " << Cname << "\n";
+	// 	cout << "Course Credit: " << Ccredit << "\n";
+	
+    // }
+}; 
+
+int BinarySearch(Course arr[],int l,int r,int x)
+  {
+
+    if (r >= l) {
+      
+    int mid = (l + r) / 2;
+
+    if (arr[mid].getSid() == x)
+      return mid;
+    
+   
+    
+    if (arr[mid].getSid() > x)
+        return BinarySearch(arr,l, mid -1, x);
+    
+    else if (arr[mid].getSid() < x)
+        return BinarySearch(arr, mid + 1,r, x);
+
+
+  }
+
+return -1;
+  }
+
+  int main()
+  {
+    
+    Course arr[10];
+	// Course s[10];
+    
+    arr[0].setCid(1);
+    arr[0].setCname("Calculus");
+    arr[0].setCcredit(0);
+    // arr[0].printCourse();
+    
+    arr[1].setCid(2);
+    arr[1].setCname("trig");
+    arr[1].setCcredit(1);
+    // arr[1].printCourse();
+    
+    arr[2].setCid(3);
+    arr[2].setCname("History");
+    arr[2].setCcredit(2);
+    // arr[2].printCourse();
+    
+    arr[3].setCid(4);
+    arr[3].setCname("Health");
+    arr[3].setCcredit(3);
+    // arr[3].printCourse();
+    
+    arr[4].setCid(5);
+    arr[4].setCname("Chemistry");
+    arr[4].setCcredit(4);
+    // arr[4].printCourse();
+    
+    arr[5].setCid(6);
+    arr[5].setCname("Biology");
+    arr[5].setCcredit(5);
+    // arr[5].printCourse();
+    
+    arr[6].setCid(7);
+    arr[6].setCname("Psychology");
+    arr[6].setCcredit(6);
+    // arr[6].printCourse();
+    
+    arr[7].setCid(8);
+    arr[7].setCname("Speech");
+    arr[7].setCcredit(7);
+    // arr[7].printCourse();
+    
+    arr[8].setCid(9);
+    arr[8].setCname("Physics");
+    arr[8].setCcredit(8);
+    // arr[8].printCourse();
+    
+    arr[9].setCid(10);
+    arr[9].setCname("English");
+    arr[9].setCcredit(9);
+    // arr[9].printCourse();
+    
+    int target = 0;
+
+    // int arr[]={1,2,3,4,5,6,7,8,9,10};
+    cout << "Enter ID of wanted course: ";
+    cin >> target;
+ 
+    int n = sizeof(arr)/sizeof(arr[0]);
+ 
+    int first = 0, last = n - 1;
+    int index = BinarySearch(arr, first, last, target);
+ 
+    if (index != -1) {
+        cout << "Course is found in the array at position: "<< target; 
+		// getCid , arr[index].getSname(), .. 
+        cout << arr[target].getSname()<< endl;
+        cout << arr[target].getSid()<< endl;
+        cout << arr[target].getCredit() << endl;
+        
+    }
+    else {
+        cout <<"Course not found in the array";
+    }
+ 
+    return 0; 
+}
+
